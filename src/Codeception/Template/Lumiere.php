b7485dbb7ee30e168b92e876ed99e4febc9f4ee6
Willington Vega <wvega@wvega.com>
2020-03-29 18:14:14 -0500

    Use fully qualified name for strpos()

--- src/Codeception/Template/Lumiere.php
+++ src/Codeception/Template/Lumiere.php
@@ -264,7 +264,7 @@ class Lumiere extends Wpbrowser {
 				$value = $value ? 'true' : 'false';
 			} elseif ( null === $value ) {
 				$value = 'null';
-			} elseif ( \is_string( $value ) && strpos( trim( $value ), ' ' ) ) {
+			} elseif ( \is_string( $value ) && \strpos( trim( $value ), ' ' ) ) {
 				$value = '"' . trim( $value ) . '"';
 			} elseif ( \is_string( $value ) ) {
 				$value = trim( $value );
