e7f5c49547aaabf7a177f4f91f424f76a0e441ca
Willington Vega <wvega@wvega.com>
2020-07-30 21:51:42 -0500

    Test that a payment method is saved

--- src/Tests/Frontend/PaymentGateways/CreditCardCest.php
+++ src/Tests/Frontend/PaymentGateways/CreditCardCest.php
@@ -6,6 +6,7 @@ use Codeception\Actor;
 use Codeception\Module\WPWebDriver;
 use SkyVerge\Lumiere\Page\Frontend\Product;
 use SkyVerge\Lumiere\Page\Frontend\Checkout;
+use SkyVerge\Lumiere\Page\Frontend\PaymentMethods;
 use SkyVerge\Lumiere\Tests\AcceptanceBase;
 
 abstract class CreditCardCest extends AcceptanceBase {
@@ -94,6 +95,86 @@ abstract class CreditCardCest extends AcceptanceBase {
 
 		$this->tester->waitForElementVisible( '.woocommerce-order-details', 30 );
 		$this->tester->see( 'Order received', '.entry-title' );
+	}
+
+
+	/**
+	 * @param Product $single_product_page Product page object
+	 * @param Checkout $checkout_page Checkout page object
+	 * @param PaymentMethods $payment_methods_page Payment Methods page object
+	 */
+	public function try_successful_transaction_for_shippable_product_saving_the_payment_method( Product $single_product_page, Checkout $checkout_page, PaymentMethods $payment_methods_page ) {
+
+		$this->tester->loginAsAdmin();
+
+		// place an order and save the payment method
+		$this->add_shippable_product_to_cart_and_go_to_checkout( $single_product_page );
+
+		$checkout_page->fillBillingDetails();
+
+		$this->place_order_and_tokenize_payment_method( $checkout_page );
+		$this->see_order_received();
+
+		// confirm the payment method is visible in the Payment Methods page
+		$token_id = $this->get_tokenized_payment_method_id();
+
+		$this->tester->amOnPage( PaymentMethods::route() );
+		$this->tester->waitForElementVisible( PaymentMethods::SELECTOR_PAYMENT_METHODS_TABLE );
+
+		$this->see_tokenize_payment_method( $token_id, $payment_methods_page );
+
+		// TODO: add a nickname to the payment method
+
+		// TODO: delete the payment method
+	}
+
+
+	/**
+	 * Places an order and thicks the Securely Save to Account checkbox.
+	 *
+	 * @param Checkout $checkout_page Checkout page object
+	 */
+	protected function place_order_and_tokenize_payment_method( Checkout $checkout_page ) {
+
+		$this->check_tokenize_payment_method_field( $checkout_page );
+		$this->place_order( $checkout_page );
+	}
+
+
+	/**
+	 * Performs the necessary steps to thicks the Securely Save to Account checkbox for the current gateway.
+	 *
+	 * @param Checkout $checkout_page Checkout page object
+	 */
+	protected function check_tokenize_payment_method_field( Checkout $checkout_page ) {
+
+		$this->tester->tryToCheckOption( str_replace( '{payment_gateway_id}', $this->get_payment_gateway_id(), Checkout::FIELD_TOKENIZE_PAYMENT_METHOD ) );
+	}
+
+
+	/**
+	 * Gets the ID of a saved payment method.
+	 *
+	 * @return int
+	 */
+	protected function get_tokenized_payment_method_id() {
+
+		// TODO: get the admin username from the configuration and make the test user configurable {WV 2020-07-30}
+		$user_id = $this->tester->grabUserIdFromDatabase( 'admin' );
+
+		return $this->tester->grabPaymentTokenIdFromDatabase( [ 'user_id' => $user_id, 'gateway_id' => $this->get_payment_gateway_id() ] );
+	}
+
+
+	/**
+	 * Confirms that a payment method row is visible the Payment Methods table
+	 *
+	 * @param int $token_id the payment method ID
+	 * @param PaymentMethods $payment_methods_page Payment Methods page object
+	 */
+	protected function see_tokenize_payment_method( int $token_id, PaymentMethods $payment_methods_page ) {
+
+		$payment_methods_page->seeTokenizedPaymentMethod( $token_id );
 	}
 
 
