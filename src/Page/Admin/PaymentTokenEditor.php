a7153d1fbdee06ef7efc6919e4d57c16977b42f1
Deiva Magalhaes <deiva@skyverge.com>
2020-08-04 16:00:47 -0700

    Add billing fields to user profile page

--- src/Page/Admin/PaymentTokenEditor.php
+++ src/Page/Admin/PaymentTokenEditor.php
@@ -14,6 +14,27 @@ class PaymentTokenEditor {
 	/** @var string base URL for the user profile page */
 	const URL = '/wp-admin/user-edit.php?user_id={user_id}';
 
+	/** @var string selector for the Billing First Name field */
+	const FIELD_BILLING_FIRST_NAME = '[name="billing_first_name"]';
+
+	/** @var string selector for the Billing Last Name field */
+	const FIELD_BILLING_LAST_NAME = '[name="billing_last_name"]';
+
+	/** @var string selector for the Billing Address field */
+	const FIELD_BILLING_ADDRESS_1 = '[name="billing_address_1"]';
+
+	/** @var string selector for the Billing City field */
+	const FIELD_BILLING_CITY = '[name="billing_city"]';
+
+	/** @var string selector for the Billing Postcode field */
+	const FIELD_BILLING_POSTCODE = '[name="billing_postcode"]';
+
+	/** @var string selector for the Billing Phone field */
+	const FIELD_BILLING_PHONE = '[name="billing_phone"]';
+
+	/** @var string selector for the Billing Email field */
+	const FIELD_BILLING_EMAIL = '[name="billing_email"]';
+
 	/** @var string selector for the Payment Tokens table */
 	const SELECTOR_PAYMENT_TOKENS_TABLE = '.sv_wc_payment_gateway_token_editor';
 
